8d9183fc685f327fe43d42330c1add26b9131fa4
Marek Bartík <bartimar6@gmail.com>
2020-09-14 15:47:13 +0200

    fix lint issues

--- pkg/worker/structs.go
+++ pkg/worker/structs.go
@@ -1,25 +1,31 @@
 package worker
 
+// Catalog holds info about all image repositories
 type Catalog struct {
 	Repositories []Repository `json:"repositories"`
 }
 
+// CatalogResponse is the json api response
 type CatalogResponse struct {
 	Repositories []string `json:"repositories"`
 }
 
+// Repository is our struct to hold parsed prefix, name, tag of image
 type Repository struct {
 	RepositoryPrefix string
 	ImageName        string
 	Tag              string
 }
 
+// TagsResponse is the json api response
 type TagsResponse struct {
 	Child    []interface{}     `json:"child"`
 	Manifest map[string]Digest `json:"manifest"`
 	Name     string            `json:"name"`
 	Tags     []string          `json:"tags"`
 }
+
+// Digest holds info about image digest
 type Digest struct {
 	ImageSizeBytes string   `json:"imageSizeBytes"`
 	LayerID        string   `json:"layerId"`
@@ -30,10 +36,12 @@ type Digest struct {
 	Name           string
 }
 
+// ListResponse is the json api response
 type ListResponse struct {
 	TagsResponses []TagsResponse `json:"tagsResponses"`
 }
 
+// Config is our application config for filtering, listing, cleaning
 type Config struct {
 	CredsFile      string   // path of credentials json file
 	RepoFilter     []string // list of regions we want to check
@@ -43,6 +51,7 @@ type Config struct {
 	RegistryURL    string
 }
 
+// FilteredList holds list of tags that were already filtered out
 type FilteredList struct {
 	TagsResponses []TagsResponse `json:"tagsResponses"`
 }
